5058cc3707affe6f88d72f22cfd5da470c2f5474
Manuel Martinez <martinez.maman25@triana.salesianos.edu>
2026-01-08 10:36:06 +0100

    Menu programa añadido

--- TrabajocolaborativoGit/src/programaColaborativo/Principal.java
+++ TrabajocolaborativoGit/src/programaColaborativo/Principal.java
@@ -1,10 +1,40 @@
 package programaColaborativo;
 
+import utilidades.Leer;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int opcion;
+		
+		System.out.println("Bienvenido, trabajo de Germán Díaz y Manuel Martínez");
+		
+		do {
+			
+			System.out.println("1. Suma");
+			System.out.println("2. Resta");
+			System.out.println("3. Multiplicación");
+			System.out.println("0. Salir");
+			opcion = Leer.datoInt();
+			
+			switch (opcion) {
+			
+			case 1:
+				
+			case 2:
+				
+			case 3:
+				
+			case 0:
+				
+			default:
+			
+			}
+			
+		} while (opcion != 0);
+		
 	}
 
 }
